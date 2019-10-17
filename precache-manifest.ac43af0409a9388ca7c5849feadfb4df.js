dc2f15c441410811ec8e71729737aad4dca81559
HankTech <hanktech1.0@gmail.com>
2019-10-17 13:47:19 -0400

    Build

---
+++ precache-manifest.ac43af0409a9388ca7c5849feadfb4df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53798da840da7a2f0c761b51721d8cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b4dd910c58f404f7e6",
+    "url": "/static/css/main.9a1bf349.chunk.css"
+  },
+  {
+    "revision": "5cc940feba8d40bf2662",
+    "url": "/static/js/2.cc24be92.chunk.js"
+  },
+  {
+    "revision": "95b4dd910c58f404f7e6",
+    "url": "/static/js/main.3f334c6e.chunk.js"
+  },
+  {
+    "revision": "f07473f367f011c62c11",
+    "url": "/static/js/runtime-main.5bc6ee3c.js"
+  }
+]);
